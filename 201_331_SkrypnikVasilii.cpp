0231d915e6812df4467caed5bd16e5664e9a17d1
Vasilii Skrypnik <VasiliiSkrypnik02@gmail.com>
2021-02-03 11:14:15 +0300

    test

---
+++ 201_331_SkrypnikVasilii.cpp
@@ -0,0 +1,10 @@
+﻿#include "Class.h"//подключение h-файла
+#include <iostream>//библиотека ввода и вывода
+using namespace std;//стандартное пространство имен
+int main()
+{
+    vector<bool> vec = { false, false, true, false };
+    printf_s("num_of_args: %d\n", num_of_args(vec));
+
+    return 0;
+}
