b37abb97361b9bf409474e6a1ffdaf687a8dcc1a
BreadOwl <71919494+BreadOwl@users.noreply.github.com>
2021-02-03 10:43:51 +0300

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,39 @@
+#include "function.h"
+#include <iostream>
+#include <vector>
+#include <string> //дл€ ввода строк
+using namespace std;
+
+int function::num_of_args(vector<bool> f) {
+	int n;
+	string f;
+	cout << "¬ведите вектор значений: " << endl;
+	vector <bool> pow; 
+	cin >> pow;
+
+	return n;
+}
+
+vector<bool> function::read_from_file(std::string file_name) {
+
+}
+
+bool function::write_to_file(std::string file_name, vector<bool> f) {
+
+}
+
+std::string function::table(vector<bool> f) {
+
+}
+
+std::string function::func_sdnf(vector<bool> f) {
+
+}
+
+std::string function::func_sknf(vector<bool> f) {
+
+}
+
+std::string function::func_Zhegalkin(vector<bool> f) {
+
+}
