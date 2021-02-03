b85ac6a9a60ab6e9b0e7359d4816beaf4b1c16c3
Egor <thewgosplayer@gmail.com>
2021-02-03 12:30:06 +0300

    Add comm to main

--- Exam1_Kulev_201_331/Exam1_Kulev_201_331.cpp
+++ Exam1_Kulev_201_331/Exam1_Kulev_201_331.cpp
@@ -6,7 +6,8 @@ int main()
     std::vector<bool> vec = {false, true, true, false, false, true, false, false};
     printf_s("num_of_args: %d\n", num_of_args(vec));
 
-    /*std::string filePathIn("D:\\vec.txt");
+    // PLEASE CHANGE FILE NAME!
+    std::string filePathIn("D:\\vec.txt");
     printf_s("Reading vector from: %s\n", filePathIn.c_str());
     std::vector<bool> inpVec = read_from_file(filePathIn);
     for (bool b : inpVec)
@@ -15,10 +16,11 @@ int main()
     }
     printf_s("\n");
 
+    // PLEASE CHANGE FILE NAME!
     std::string filePathOut("D:\\vec_out.txt");
     printf_s("Writing vector to: %s\n", filePathOut.c_str());
     bool result = write_to_file(filePathOut, vec);
-    printf_s("Write result: %d (%s)\n", result, result ? "success" : "error");*/
+    printf_s("Write result: %d (%s)\n", result, result ? "success" : "error");
 
     std::string fTable = table(vec);
     printf_s("Table:\n%s\n", fTable.c_str());
