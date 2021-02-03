c73f1b5f21082da10be66d305a5773ca3638b721
Valeria-Ya <71924939+Valeria-Ya@users.noreply.github.com>
2021-02-03 17:42:31 +0400

    Add files via upload

--- Functions.h
+++ Functions.h
@@ -9,3 +9,6 @@ vector<bool> read_from_file(string file_name);
 bool write_to_file(string file_name, vector<bool> f);
 string table(vector<bool> f);
 
+vector<int> func_Pascal(int k);
+int func_substr_len(string input_str);
+int roman_to_arab(string roman_number);
--- Yagubyan_Valeria_201_331.cpp
+++ Yagubyan_Valeria_201_331.cpp
@@ -1,9 +1,10 @@
 ﻿// Yagubyan_Valeria_201_331.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include <iostream>
-#include <string>
-#include <vector> //библиотека векторов
+#include <iostream> //библиотека для ввода-вывода
+#include <string> //библиотека для работы с с++ строками
+#include<set> //библиотека set
+#include <vector> //библиотека для работы с векторами
 #include <fstream> //методы и функции для чтения/записи файла
 using namespace std;
 
@@ -22,7 +23,6 @@ bool write_to_file(string file_name, vector<bool> f)
     {
         file << line << " ";
     }
-
     return true;
 }
 
@@ -68,16 +68,103 @@ string table(vector<bool> f)
 // имя файла C:\Users\Персик\Desktop\Test.txt
 
 
+vector<int> func_Pascal(int k)
+{
+        vector<int> vector;
+        int i, j;
+        vector.push_back(1);
+        for (i = 1; i <= k; i++) 
+            vector.push_back(0);
+        for (j = 1; j <= k; j++)
+            for (i = j; i >= 1; i--)
+                vector[i] = vector[i - 1] + vector[i];
+            return vector;
+}
+
+
+int func_substr_len(string input_str)
+{
+    set<char> symbol; // хранение уникальных букв
+    int normal_size = 0, max_size = 0;
+    for (int i = 0; i < input_str.length(); i++) {
+        symbol.insert(input_str[i]);
+        if (symbol.size() > normal_size) // проверка на повторение
+            normal_size = symbol.size();
+        else {
+            if (normal_size > max_size) max_size = normal_size;
+            symbol.clear();
+            normal_size = symbol.size();
+        }
+    }
+    if (normal_size > max_size) max_size = normal_size; // если введенная строка заканчивается на подстроку, 
+    // которая содержит неповторяющиеся символы - проверка данной ситуации
+
+    return max_size;
+}
+int roman_to_arab(string roman_number)
+{
+        int res = 0;
+        for (int i = 0; i < roman_number.length(); i++)
+        {
+            if (roman_number[i] == 'M')
+                res += 1000;
+            if (roman_number[i] == 'C' && roman_number[i + 1] == 'M')
+            {
+                res += 900;
+                i++;
+            }
+            if (roman_number[i] == '‎D')
+                res += 500;
+            if (roman_number[i] == 'C' && roman_number[i + 1] == 'D')
+            {
+                res += 400;
+                i++;
+            }
+            if (roman_number[i] == 'C')
+                res += 100;
+            if (roman_number[i] == 'X' && roman_number[i + 1] == 'C')
+            {
+                res += 90;
+                i++;
+            }
+            if (roman_number[i] == 'L')
+                res += 50;
+            if (roman_number[i] == 'X' && roman_number[i + 1] == 'L')
+            {
+                res += 40;
+                i++;
+            }
+            if (roman_number[i] == 'X')
+                res += 10;
+            if (roman_number[i] == 'I' && roman_number[i + 1] == 'X')
+            {
+                res += 9;
+                i++;
+            }
+            if (roman_number[i] == 'V')
+                res += 5;
+            if (roman_number[i] == 'I' && roman_number[i + 1] == 'V')
+            {
+                res += 4;
+                i++;
+            }
+            if (roman_number[i] == 'I')
+                res += 1;
+        }
+        return res;
+}
+
+
 int main()
 {
-    int number;
-    bool data;
+    int number, n, i;
     vector<bool> f;
-    string file_name;
+    string file_name, string, rim_number;
     system("chcp 1251 > nul"); //кодировка
     vector<bool> bool_vec = {false, true, false, true};
     cout << "Введите полный путь к файлу" << endl;
     getline(cin, file_name);
+
     while (true)
     {
         cout << "Введите номер задания:" << endl;
@@ -85,7 +172,10 @@ int main()
         cout << "2. вернуть вектор значений булевой функции" << endl;
         cout << "3. записать заданный вектор значений f булевой функции в файл, заданный расположением и имененем" << endl;
         cout << "4. Таблица истинности" << endl;
-        cout << "5. Выход" << endl;
+        cout << "5. Треугольник Паскаля" << endl;
+        cout << "6. Вернуть число, равное размеру самой длинной подстроки, которая не содержит повторяющихся символов" << endl;
+        cout << "7. Римские в арабские" << endl;
+        cout << "8. Выход" << endl;
         cin >> number;
         switch (number)
         {
@@ -110,6 +200,29 @@ int main()
             break;
         }
         case 5:
+        {
+            cout << "Введите k" << endl;
+            cin >> n;
+            vector<int> vector = func_Pascal(n);
+            for (i = 0; i < n+1; i++)
+                cout << vector[i] << " ";
+            break;
+        }
+        case 6:
+        {
+            cout << "Введите строку" << endl;
+            cin >> string;
+            cout << func_substr_len(string);
+            break;
+        }
+        case 7:
+        {
+            cout << "Введите римское число" << endl;
+            cin >> rim_number;
+            cout << roman_to_arab(rim_number) << endl;
+            break;
+        }
+        case 8:
         {
             return 0;
         }
