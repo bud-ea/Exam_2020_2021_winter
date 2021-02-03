46156fce4c65f5aa8abf442b1b9b2073571de1a2
Zholeg <69371601+Zholrg@users.noreply.github.com>
2021-02-03 15:45:37 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 #include "table.h"
 using namespace std;
-string nameout = "base.txt";
+/*string nameout = "base.txt";
 ifstream input;
 ofstream out;
 
@@ -119,14 +119,197 @@ int main()
         case 0:
             return 0;
         }
+    }*/
+/*input >> a;
+input.close();
+out.open("base.txt");
+out << a/4;
+out.close();
+input.open("base.txt");
+input >> a;
+cout << a;*/
+int main()
+{
+for(;;){
+    string Cesar;
+    cin >> Cesar;
+    int dec_num=0,c=1;
+    char cur_symb='0';
+    for(int i=0; i<Cesar.size(); ++i)
+    {
+        if(Cesar[i]=='I' and Cesar[i+1]=='V')
+        {
+            switch(cur_symb)
+            {
+            case 'I':
+                dec_num+=1*c;
+                break;
+            case 'V':
+                dec_num+=5;
+                break;
+            case 'X':
+                dec_num+=10*c;
+                break;
+            case 'L':
+                dec_num+=50;
+                break;
+            case 'C':
+                dec_num+=100*c;
+                break;
+            case 'D':
+                dec_num+=500;
+                break;
+            case 'M':
+                dec_num+=1000*c;
+                break;
+            default:
+                break;
+
+            }
+            dec_num+=4;
+            c=1;
+            i++;
+        }
+            else if(Cesar[i]=='X' and Cesar[i+1]=='L')
+        {
+            switch(cur_symb)
+            {
+            case 'I':
+                dec_num+=1*c;
+                break;
+            case 'V':
+                dec_num+=5;
+                break;
+            case 'X':
+                dec_num+=10*c;
+                break;
+            case 'L':
+                dec_num+=50;
+                break;
+            case 'C':
+                dec_num+=100*c;
+                break;
+            case 'D':
+                dec_num+=500;
+                break;
+            case 'M':
+                dec_num+=1000*c;
+                break;
+            default:
+                break;
+
+            }
+            dec_num+=40;
+            c=1;
+            i++;
+        }
+            else if(Cesar[i]=='C' and Cesar[i+1]=='D')
+        {
+            switch(cur_symb)
+            {
+            case 'I':
+                dec_num+=1*c;
+                break;
+            case 'V':
+                dec_num+=5;
+                break;
+            case 'X':
+                dec_num+=10*c;
+                break;
+            case 'L':
+                dec_num+=50;
+                break;
+            case 'C':
+                dec_num+=100*c;
+                break;
+            case 'D':
+                dec_num+=500;
+                break;
+            case 'M':
+                dec_num+=1000*c;
+                break;
+            default:
+                break;
+
+            }
+            dec_num+=400;
+            c=1;
+            i++;
+        }
+        else
+        {
+        cout << " g ";
+            if(cur_symb!=Cesar[i])
+            {
+                switch(cur_symb)
+                {
+                case 'I':
+                    dec_num+=1*c;
+                    break;
+                case 'V':
+                    dec_num+=5;
+                    break;
+                case 'X':
+                    dec_num+=10*c;
+                    break;
+                case 'L':
+                    dec_num+=50;
+                    break;
+                case 'C':
+                {cout << c*100;
+                    dec_num+=100*c;
+                    break;}
+                case 'D':
+                    dec_num+=500;
+                    break;
+                case 'M':
+                {cout << c*1000 << " ";
+                    dec_num+=1000*c;
+                    break;}
+                default:
+                    break;
+
+                }
+                cur_symb=Cesar[i];
+                c=1;
+            }
+            else
+                c++;
+            if(i+1>=Cesar.size())
+            {
+                switch(cur_symb)
+                {
+                case 'I':
+                    dec_num+=1*c;
+                    break;
+                case 'V':
+                    dec_num+=5;
+                    break;
+                case 'X':
+                    dec_num+=10*c;
+                    break;
+                case 'L':
+                    dec_num+=50;
+                    break;
+                case 'C':
+                {cout << c*100;
+                    dec_num+=100*c;
+                    break;}
+                case 'D':
+                    dec_num+=500;
+                    break;
+                case 'M':
+                 {cout << c*1000 << 1000 << " ";
+                    dec_num+=1000*c;
+                    break;}
+                default:
+                    break;
+
+                }
+            }
+        }
+
     }
-    /*input >> a;
-    input.close();
-    out.open("base.txt");
-    out << a/4;
-    out.close();
-    input.open("base.txt");
-    input >> a;
-    cout << a;*/
+    cout << dec_num;}
     return 0;
 }
