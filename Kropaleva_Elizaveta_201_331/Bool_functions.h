0990fd93becac37f1781fa8cf6e5b58a867cda8e
ElizavetaKropaleva <74456599+ElizavetaKropaleva@users.noreply.github.com>
2021-02-03 12:34:47 +0300

    final commit

---
+++ Kropaleva_Elizaveta_201_331/Bool_functions.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector> // библиотека для работы с vector
+#include <string> // библиотека для работы с string
+
+
+using namespace std;
+
+int num_of_args(vector<bool> f);
+
+vector<bool> read_from_file(std::string file_name);
+
+bool write_to_file(std::string file_name, vector<bool> f);
+
+std::string table(vector<bool> f);
---
+++ Kropaleva_Elizaveta_201_331/Kropaleva_Elizaveta_201_331.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector> // библиотека для работы с vector
+#include "Bool_functions.h" // заголовочный файл с написанным функциями 
+
+using namespace std;
+
+int main()
+{
+	vector<bool> f = { 0, 1, 0 , 0, 1, 1, 0, 0 };
+	vector<bool> f3 = { 0, 1, 0 , 0};
+	cout << num_of_args(f) << endl << endl;
+
+	vector <bool> f1 = read_from_file("D:\\vector.txt");
+	for (int i = 0; i < f1.size(); i++) cout << f1[i] << "\t";
+	cout << endl << endl;
+
+	if (write_to_file("D:\\vector_w.txt", f));
+
+	cout << table(f) << endl;
+}
