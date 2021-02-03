b73b4fd4795906d7ef07209b7133ccc48d566e6b
VickyMosh <71924923+VickyMosh@users.noreply.github.com>
2021-02-03 10:49:34 +0300

    Add files via upload

---
+++ Moshnikova_Victoria_201_331.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>                    // ввод и вывод
+#include <vector>                     // работа с векторами
+#include <math.h>                    // мат. операции
+using namespace std;
+vector<bool> f = { 0, 0, 0, 1, 1, 0, 1, 1 };  //то, что было дано 
+int num_of_args(vector<bool> f)           
+{
+	int n = log2(f.size());                  //степень двойки опред   
+	return n;
+}
+int main() 
+{
+	int n = num_of_args(f);
+	cout << n;                          //должен вывести 3 
+}
