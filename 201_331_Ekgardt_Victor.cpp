cd8dcdd3f1b4f26c6a6e2b8141fb241117d86d8b
v279e <71925316+v279e@users.noreply.github.com>
2021-02-03 10:54:58 +0300

    Add files via upload

---
+++ 201_331_Ekgardt_Victor.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>// библиотека ввода/вывода
+#include <fstream>// работа с файлами
+#include "Header1.h"// подключаем h.file
+#include "Source1.cpp"// подключаем cpp.file
+using namespace std;
+int main()
+{
+	vector<bool> vec = { false, true, true, false };
+	printf_s("num_of_args: %d\n", num_of_args(vec));
+}
---
+++ Source1.cpp
@@ -0,0 +1,6 @@
+#include "Header1.h"
+
+int num_of_args(std::vector<bool> f)
+{
+	return (int)log2(f.size());;
+}
