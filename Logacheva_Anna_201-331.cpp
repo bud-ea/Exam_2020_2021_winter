b385cb53fe82fa072b52e0c74126cb1d12affc92
AnnLogacheva <72224593+AnnLogacheva@users.noreply.github.com>
2021-02-03 12:22:56 +0300

    Update Logacheva_Anna_201-331.cpp

--- Logacheva_Anna_201-331.cpp
+++ Logacheva_Anna_201-331.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
 #include <vector>//для вектора
 #include "Logacheva_Anna_201-331.h"
+#include "Source.cpp"
+#include <fstream>
 using namespace std;
 int main()
 {
     setlocale(LC_ALL, "rus");
     int num_of_args(vector<bool> f);
+    vector<bool> read_from_file(string file_name);
+    std::string table(vector<bool> f);
 
-
-
-    //int num_of_args(vector<bool> f) done
-	//vector<bool> read_from_file (std::string file_name)
-	//bool write_to_file (std::string file_name, vector<bool> f)
-	//std::string table (vector<bool> f)
+        //int num_of_args(vector<bool> f) done
+	//vector<bool> read_from_file (std::string file_name)done
+	//bool write_to_file (std::string file_name, vector<bool> f)done
+	//std::string table (vector<bool> f)done
 	//std::string func_sdnf (vector<bool> f)
 	//std::string func_sknf (vector<bool> f)
 	//std::string func_Zhegalkin (vector<bool> f)
-}
-
-int num_of_args(vector<bool> f)
-{
-    //кол-во аргументов в функции
-    int n = 1;
-    vector <int> f = { 0,0,0,1,0,0,0,1, };
-    while (n != f.size())
-        n = n * 2;
-    return n / 2;
 }
