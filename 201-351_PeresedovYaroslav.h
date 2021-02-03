4dbaf9fbed58fc4f766351e4e4f8256d567a33bd
Yaroslav-Peresedov <78450479+Yaroslav-Peresedov@users.noreply.github.com>
2021-02-03 12:33:22 +0300

    Add files via upload

--- 201-351_PeresedovYaroslav.h
+++ 201-351_PeresedovYaroslav.h
@@ -9,5 +9,10 @@ using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
-vector <bool> read_from_file(string file_name, vector <bool> f);
+
 int num_of_args(vector <bool> f);
+vector <bool> read_from_file(string file_name, vector <bool> f);
+string advance(string arguments);
+string table(vector <bool> f);
+string func_sdnf(vector <bool> f);
+string func_sknf(vector <bool> f);
