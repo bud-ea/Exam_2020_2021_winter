2ccd1803c29143374ca46481281d846b8fbe9691
Vladislav-111 <72132247+Vladislav-111@users.noreply.github.com>
2021-02-03 16:39:19 +0300

    Часть 2

--- 201-352_Poletaev_Vlad.cpp
+++ 201-352_Poletaev_Vlad.cpp
@@ -1,7 +1,7 @@
 
 #include <iostream>//стандартная библиотека ввода - вывода
 #include <vector>//добавление библиотеки вектора
-#include "Funcions.h"//добавление .h файла в программу
+#include "Funcions.h"//добавление функций h файла в программу
 #include <string>// добавление библиотеки string
 #include <iterator>//библиотека управления значениями для вектора
 
@@ -12,8 +12,9 @@ using std::string;
 
 int main()
 {
-	//возврат количество аргументов n функции f (task 1)
 	/*
+	//возврат количество аргументов n функции f (task 1)
+	
 	vector<bool>f={0};
 	bool fun;
 	int n;
@@ -23,29 +24,55 @@ int main()
 	}
 	num_of_args(f, n);
 	cout <<"Length of vector ="<< pow(2, n);
-	*/
+	
 
 	//записать вектор значений в файл(task 3)
-	/*
+	
 	vector<bool> f = { 0,1,0,1,0 };
 	string file_names;
 	vector<bool>::iterator it;
 	for (*it = f.begin();; it++) {
-		
+
 	}
-	*/
+	
 
 
 
 	//по вектору вывести таблицу истиности (task 4)
-	/*
-	string file_name=("fewbfh fw 0 0 1 1");
-	vector<bool> file;
+	int n = 4;
+	bool *arr = new bool[n];
+	string file_name=("0 1 0 1");
+	string x1, x2, x3, x4;
 	
-	for (int i = 0; i < file_name.length(); i++) {
-		if (i == int("0") && i == int("1")) {
-
-		}
+	for (int i = 0; i < n; i++){
+		
 	}
+
+	delete[] arr;
 	*/
+
+	
+	//Часть 2
+	
+	//перевести римские цифры в арабские
+	char roman_number[100]{};
+	int number=0;
+	int n;
+
+	cout << "Enter lot of number: ";
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cout << "Enter I,V,X,L,C,D or M";
+		cin >> roman_number[i];
+		roman_to_arab(roman_number[i], n);//смена числа
+		number = number+roman_number[i];
+	}
+	cout << number;
+
+	//треугольник Паскаля
+	int k;
+	cout << "Enter number: ";
+	cin >> k;
+	fun_paskal(k);//вывод k строки
+	
 }
--- Funcions.h
+++ Funcions.h
@@ -1,11 +1,14 @@
 #pragma once
+#include <iostream>
 #include <string>
 #include <vector>
+
 
 using std::vector;
 using std::string;
 
 
+/*
 int num_of_args(vector<bool> f,int n)//возврат количество аргументов n функции f
 {
 	n = f.size();
@@ -21,4 +24,48 @@ bool write_to_file(string file_name,vector<bool> f)
 		return false;
 }
 
+string table(bool *arr)
+{
 
+}
+*/
+
+//Часть 2
+int roman_to_arab(char roman_number,int n)
+{
+	int I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000;
+
+		if (roman_number == I)
+			roman_number = I;
+		if (roman_number == V)
+			roman_number = V;
+		if (roman_number == X)
+			roman_number = X;
+		if (roman_number == L)
+			roman_number = L;
+		if (roman_number == C)
+			roman_number = C;
+		if (roman_number == D)
+			roman_number = D;
+		if (roman_number == M)
+			roman_number = M;
+		
+		return roman_number;
+}
+
+
+int fun_paskal(int k)
+{
+	if (k > 0 && k <= 33) {
+		int arr[1000]{};
+		for (int i = 1; i < k; i++) {
+			arr[0] = 1;
+			for (int j = 1; j < k; j++) {
+				for (i = j; i >= 1; i--) {
+					arr[i] = arr[i - 1] + arr[i];
+				}
+			}
+		}
+	}
+	return 0;
+}
