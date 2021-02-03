71e566ba8a4aa1c373318223b3b1b28ac90a2c62
v279e <71925316+v279e@users.noreply.github.com>
2021-02-03 12:43:44 +0300

    Add files via upload

--- 201_331_Ekgardt_Victor.cpp
+++ 201_331_Ekgardt_Victor.cpp
@@ -1,10 +1,20 @@
-﻿#include <iostream>// библиотека ввода/вывода
-#include <fstream>// работа с файлами
-#include "Header1.h"// подключаем h.file
-#include "Source1.cpp"// подключаем cpp.file
+﻿#include <iostream> //ввод вывод
+#include <vector> // библиотека для работы с vector
+#include "Header1.h" // заголовочный файл с написанным функциями 
+
 using namespace std;
+
 int main()
 {
-	vector<bool> vec = { false, true, true, false };
-	printf_s("num_of_args: %d\n", num_of_args(vec));
+	vector<bool> f = { 0, 1, 0 , 0, 1, 1, 0, 0 };
+	vector<bool> f3 = { 0, 1, 0 , 0 };
+	cout << num_of_args(f) << endl << endl;
+
+	vector <bool> f1 = read_from_file("D:\\vector.txt");
+	for (int i = 0; i < f1.size(); i++) cout << f1[i] << "\t";
+	cout << endl << endl;
+
+	if (write_to_file("D:\\vector_w.txt", f));
+
+	cout << table(f) << endl;
 }
--- Header1.h
+++ Header1.h
@@ -1,2 +1,8 @@
-#pragma once
-#include <vector>
+﻿#pragma once
+#include <vector> // работа с векторами
+#include <string> // c строки
+using namespace std;
+int num_of_args(vector<bool> f);
+vector<bool> read_from_file(std::string file_name);
+bool write_to_file(std::string file_name, vector<bool> f);
+std::string table(vector<bool> f);
