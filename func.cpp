ad7e997c8246f9d73112f8e5680e318d52344e89
VldSann <71924927+VldSann@users.noreply.github.com>
2021-02-03 12:39:40 +0300

    Add files via upload

--- func.cpp
+++ func.cpp
@@ -1,60 +1,42 @@
 #include <iostream> // cout cin
 #include <vector> // для работы с векторами
 #include <math.h> // для математических операций
-#include <fstream>
-#include <string>
+#include <fstream> // для чтения/записи данных из/в файл
+#include <string> // для работы со строками
+#include "func.h"
 
 using namespace std;
 
-const string file = {"F:\\file.txt"}; // расположение файла1
-const string file2 = {"F:\\file2.txt"}; // расположение файла2
+const string file = {"D:\\file.txt"}; // расположение файла1
+const string file2 = {"D:\\file2.txt"}; // расположение файла2
 
-vector<bool> f; // вектор по умолчанию
+vector <bool> f; // вектор по умолчанию
 
-vector<bool> read_from_file(string file_name) { // чтение значений из файла
-    vector<bool> vec; // вектор который возвращаем
-    string str;
-    ifstream out(file_name);
-    if (!out.is_open()) {
-        cout << "Ошибка открытия файла!\n";
-    } else {
-        while (!out.eof()) { // цикл считывания значений
-            str = "";
-            out >> str;
-            if (str == "") continue;
-            vec.push_back(stoi(str)); // передача значений в вектор
-        }
-    }
-    out.close(); // закрываем файл
-    return vec;
-}
 
-bool write_to_file(std::string file_name, vector<bool> f) {
-    ofstream in;
-    bool v;
-    in.open(file_name, ios::app);
-    int i;
-    for (i = 0; i < f.size() - 1; i++) {
-        in << f[i] << " ";
-    }
-    in << f[i];
-    in.close();
-    vector<bool> vec;
-    vec = read_from_file(file_name);
-    if (vec == f) {
-        v = true;
-    }
-
-    return v;
-}
-
-int num_of_args(vector<bool> f) {
-    int n = log2(f.size());
-    return n;
-}
 
 int main() {
+    setlocale(0, " ");
     f = read_from_file(file);
     bool tr = write_to_file(file2, f);
-    cout << tr;
+    int a = num_of_args(f);
+    cout << "1 Задание: " << a << endl;
+    cout << "2 Задание: ";
+    for (int i = 0; i < f.size(); i++){
+        cout << f[i];
+    }
+    cout << endl;
+    cout << "3 Задание:";
+    if (tr == 1){
+        cout << "True";
+    } else{
+        cout << "False";
+    }
+    cout << endl;
+    cout << "4 задание: ";
+    string string1 = table(f);
+    cout << string1;
+    cout << endl;
+    cout << "5 Задание: ";
+    string string2 = func_sdnf(f);
+    cout << string2;
 }
--- func.h
+++ func.h
@@ -1,8 +1,9 @@
 #include <iostream> // cout cin
 #include <vector> // для работы с векторами
 #include <math.h> // для математических операций
-#include <fstream>
-#include <string>
+#include <fstream> // для чтения/записи данных из/в файл
+#include <string> // для работы с Си-строками
+
 using namespace std;
 
 int num_of_args(vector<bool> f) {
@@ -10,7 +11,7 @@ int num_of_args(vector<bool> f) {
     return n;
 }
 
-vector <bool> read_from_file(string file_name) { // чтение значений из файла
+vector<bool> read_from_file(string file_name) { // чтение значений из файла
     vector<bool> vec; // вектор который возвращаем
     string str;
     ifstream out(file_name);
@@ -28,7 +29,7 @@ vector <bool> read_from_file(string file_name) { // чтение значений из файла
     return vec;
 }
 
-bool write_to_file(std::string file_name, vector <bool> f) {
+bool write_to_file(std::string file_name, vector<bool> f) {
     ofstream in;
     bool v;
     in.open(file_name, ios::app);
@@ -38,7 +39,7 @@ bool write_to_file(std::string file_name, vector <bool> f) {
     }
     in << f[i];
     in.close();
-    vector <bool> vec;
+    vector<bool> vec;
     vec = read_from_file(file_name);
     if (vec == f) {
         v = true;
@@ -47,18 +48,56 @@ bool write_to_file(std::string file_name, vector <bool> f) {
     return v;
 }
 
-std::string table (vector<bool> f){
+string table(vector<bool> f) {
+    string table;
+    int num = num_of_args(f);
 
+    for (int i = 1; i <= num; i++) {
+        table += "x" + to_string(i) + "\t"; // Вывод шапки
+    }
+    table += "f\n";
+
+    for (int i = 0; i < f.size(); i++) {
+        for (int j = 0; j <= num; j++) {
+            if (j == num)
+                table += to_string(f.at(i));
+            else
+                table += to_string((bool) (i & (int) (pow(2, (num - j - 1))))) + "\t";
+        }
+        table += "\n";
+    }
+    return table;
 }
 
-std::string func_sdnf (vector<bool> f){
+std::string func_sdnf(std::vector<bool> f) {
+    string func("f_sdnf(");
+    int num = num_of_args(f); // получаем количество аргументов
+    bool has_s_func = false; // используется для обработки, если функция имеет SDNF
 
-}
+    for (int i = 1; i <= num; i++) {
+        func += "x" + std::to_string(i);
+        if (i != num)
+            func += ", ";
+    }
+    func += ") = ";
 
-std::string func_sknf (vector<bool> f){
+    for (int i = 0; i < f.size(); i++) {// перебираем все значения функции
+        // SDNF строится только для истинных значений функции!
+        if (f.at(i)) { // сообщаем, что функция действительно имеет sdnf
+            has_s_func = true;
+            func += "(";
+            for (int j = 0; j < num; j++) { // пройти по таблице истинности
+                // тот же метод получения значения для аргумента, который использовался выше
+                bool arguments = (bool) (i & (int) (pow(2, (num - j - 1))));
+                // обрабатываем, если значение аргумента ложно
+                if (!arguments)
+                    func += "'";
 
-}
-
-std::string func_Zhegalkin (vector<bool> f){
-
+                func += "x" + std::to_string(j + 1); //выводим элемент формулы
+                if (j != num - 1)
+                    func += " & ";
+            }
+            func += ") v ";
+        }
+    }
 }
