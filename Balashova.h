763549c8f291fede64c60de74cfa42a368bb514b
Dybashka <72248943+Dybashka@users.noreply.github.com>
2021-02-03 16:27:06 +0300

    Add files via upload

--- Balashova.h
+++ Balashova.h
@@ -5,6 +5,7 @@
 #include <fstream>
 using namespace std;
 
+//×ÀÑÒÜ 1
 int num_of_args(vector<bool> f);
 vector<bool> read_from_file(string file_name);
 bool write_to_file(string file_name, vector<bool> f);
@@ -12,3 +13,9 @@ string table(vector<bool> f);
 string func_sdnf(vector<bool> f);
 string func_sknf(vector<bool> f);
 string func_Zhegalkin(vector<bool> f);
+
+//×ÀÑÒÜ 2
+int roman_to_arab(string roman_number);
+unsigned long long make_C(int k, int n);
+vector<int>func_Pascal(int k);
+int func_substr_len(string input_str);
