8e47dec2594601d4512212c07a2b10cfa61f97a7
AnnLogacheva <72224593+AnnLogacheva@users.noreply.github.com>
2021-02-03 16:37:34 +0300

    Update Logacheva_Anna_201-331.h

--- Logacheva_Anna_201-331.h
+++ Logacheva_Anna_201-331.h
@@ -7,3 +7,6 @@ int num_of_args (vector<bool> f);
 vector<bool> read_from_file(string file_name); 
 bool write_to_file(string file_name, vector<bool> f);
 std::string table(vector<bool> f);
+// вторая часть экзамена 
+int roman_to_arab(std::string roman_number);
+vector<int>func_Pascal(int k);
