08604d81816b370d4cfdad674929c2ae41480be8
FDI-debug <53896920+FDI-debug@users.noreply.github.com>
2021-02-03 12:36:00 +0300

    Add files via upload

--- 201_331_Gordeychuk_Nikita/Gordeychuk_Nikita_all_func.h
+++ 201_331_Gordeychuk_Nikita/Gordeychuk_Nikita_all_func.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
+//#include <vector>
+//#include <string>
+#include <fstream> //работа с файлами
 int num_of_arguments(std::vector <bool> f) {
 	int len = f.size();
 	int answ;
@@ -12,20 +12,22 @@ int num_of_arguments(std::vector <bool> f) {
 	}
 	return answ;
 }
-vector <bool> read_from_file(string file_name) {
-	ifstream file;
-	vector <bool> output;
-	char buff[16];
+std::vector <bool> read_from_file(std::string file_name) {
+	std::ifstream file;
+	std::vector <bool> output;
+	std::string str;
 	file.open(file_name);
-	file >> buff;
-	output.push_back(buff);
+	getline(file, str, '\0');
 	file.close();
+	for (auto ch : str) {
+		output.push_back(ch);
+	}
 	return output;
 }
-bool write_to_file(std::string file_name, vector <bool> f) {
-	ofstream file;
+bool write_to_file(std::string file_name, std::vector <bool> f) {
+	std::ofstream file;
 	file.open(file_name);
-	if (!file.is_open) {
+	if (!file) {
 		return false;
 	}
 	for (auto data : f) {
@@ -33,4 +35,118 @@ bool write_to_file(std::string file_name, vector <bool> f) {
 	}
 	file.close();
 	return true;
+}
+std::string table(std::vector <bool> f) {
+	std::string answ_task_4 = "x1\tx2\tx3\tx4\tF\n";
+	std::string x1 = "0000000011111111";
+	std::string x2 = "0000111100001111";
+	std::string x3 = "0011001100110011";
+	std::string x4 = "0101010101010101";
+	std::string F;
+	for (auto num : f) {
+		F += char(num);
+	}
+	for (int i = 0; i < 16; i++) {
+		answ_task_4 += char(x1[i]);
+		answ_task_4 += '\t';
+		answ_task_4 += char(x2[i]);
+		answ_task_4 += '\t';
+		answ_task_4 += char(x3[i]);
+		answ_task_4 += '\t';
+		answ_task_4 += char(x4[i]);
+		answ_task_4 += '\t';
+		answ_task_4 += char(F[i]);
+		answ_task_4 += '\n';
+	}
+	return answ_task_4;
+}
+std::string func_sdnf(std::vector <bool> f) {
+	std::string answ;
+	int x1[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 };
+	int x2[16] = { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1 };
+	int x3[16] = { 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 };
+	int x4[16] = { 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 };
+	for (int i = 0; i < 16; i++) {
+		if (f[i] == 1) {
+			if (x1[i] == 0) {
+				answ += "'x1";
+				answ += "&";
+			}
+			else if (x1[i] == 1) {
+				answ += "x1";
+				answ += "&";
+			}
+			if (x2[i] == 0) {
+				answ += "'x2";
+				answ += "&";
+			}
+			else if (x2[i] == 1) {
+				answ += "x2";
+				answ += "&";
+			}
+			if (x3[i] == 0) {
+				answ += "'x3";
+				answ += "&";
+			}
+			else if (x3[i] == 1) {
+				answ += "x3";
+				answ += "&";
+			}
+			if (x4[i] == 0) {
+				answ += "'x4";
+			}
+			else if (x4[i] == 1) {
+				answ += "x4";
+			}
+			if (i < 15) {
+				answ += " V ";
+			}
+		}
+	}
+	return answ;
+}
+std::string func_sknf(std::vector <bool> f) {
+	std::string answ;
+	int x1[16] = { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 };
+	int x2[16] = { 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1 };
+	int x3[16] = { 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 };
+	int x4[16] = { 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1 };
+	for (int i = 0; i < 16; i++) {
+		if (f[i] == 0) {
+			if (x1[i] == 1) {
+				answ += "'x1";
+				answ += "V";
+			}
+			else if (x1[i] == 0) {
+				answ += "x1";
+				answ += "V";
+			}
+			if (x2[i] == 1) {
+				answ += "'x2";
+				answ += "V";
+			}
+			else if (x2[i] == 0) {
+				answ += "x2";
+				answ += "V";
+			}
+			if (x3[i] == 1) {
+				answ += "'x3";
+				answ += "V";
+			}
+			else if (x3[i] == 0) {
+				answ += "x3";
+				answ += "V";
+			}
+			if (x4[i] == 1) {
+				answ += "'x4";
+			}
+			else if (x4[i] == 0) {
+				answ += "x4";
+			}
+			if (i < 15) {
+				answ += " & ";
+			}
+		}
+	}
+	return answ;
 }
--- 201_331_Gordeychuk_Nikita/Gordeychuk_Nikita_all_tasks.cpp
+++ 201_331_Gordeychuk_Nikita/Gordeychuk_Nikita_all_tasks.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "functions.h"
+#include "Gordeychuk_Nikita_all_func.h"
 using namespace std;
 
 int main() {
+	setlocale(LC_ALL, "ru");
 	//____________________«¿ƒ¿Õ»≈ 1_________________________
 	int answ_task_1;
 	vector <bool> task_1 = { 0, 0, 1, 1 };
 	answ_task_1 = num_of_arguments(task_1);
-	cout << answ_task_1;
+	cout << answ_task_1 << endl;
 	//____________________«¿ƒ¿Õ»≈ 2_________________________
 	string read_file_name = "read_bool_vector.txt";
 	vector <bool> answ_task_2 = read_from_file(read_file_name);
@@ -17,6 +18,21 @@ int main() {
 		cout << data << ' ';
 	}
 	//____________________«¿ƒ¿Õ»≈ 3_________________________
-	string write_file_name = "";
+	string write_file_name = "write_bool_vector.txt";
+	vector <bool> f = { 0, 0, 0, 0, 1, 1, 1, 1 };
+	cout << write_to_file(write_file_name, f);
+	cout << endl;
+	//____________________«¿ƒ¿Õ»≈ 4_________________________
+	vector <bool> task_4 = { 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 };
+	string answ_task_4 = table(task_4);
+	cout << answ_task_4 << endl;
+	//____________________«¿ƒ¿Õ»≈ 5_________________________
+	vector <bool> f_sdnf = { 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 };
+	string str_task_5 = func_sdnf(f_sdnf);
+	cout << str_task_5 << endl;
+	//____________________«¿ƒ¿Õ»≈ 6_________________________
+	vector <bool> f_sknf = { 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1 };
+	string str_task_6 = func_sknf(f_sknf);
+	cout << str_task_6 << endl;
 	return 0;
 }
