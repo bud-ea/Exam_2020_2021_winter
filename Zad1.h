b41f8ed28142a3ced3ffe9123c5791e001fd8baf
Nikorambus <72225007+Nikorambus@users.noreply.github.com>
2021-02-03 16:43:20 +0300

    Update Zad1.h

--- Zad1.h
+++ Zad1.h
@@ -1,7 +1,7 @@
-﻿#pragma once
+#pragma once
 #include <vector> 
 #include <string> 
-
+#include <unordered_set>
 
 using namespace std;
 
@@ -10,3 +10,9 @@ int num_of_args(vector<bool> f);
 vector<bool> read_from_file(std::string file_name);
 
 bool write_to_file(std::string file_name, vector<bool> f);
+
+int roman_to_arab(std::string roman_number);
+
+int func_substr_len(string input_string);
+
+std::vector<int> func_Pascal(int k);
