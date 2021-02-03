780b1f7acdba98a6b6373b175b94e9db58c7517b
Nikorambus <72225007+Nikorambus@users.noreply.github.com>
2021-02-03 16:44:12 +0300

    Update Egel_Nikita_201-331_Ekz.cpp

--- Egel_Nikita_201-331_Ekz.cpp
+++ Egel_Nikita_201-331_Ekz.cpp
@@ -1,14 +1,40 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>		// библиотека для работы с vector
-#include "Zad1.h"		// заголовочный файл с написанным функциями 
+#include <string>		// библиотека для работы с с++ строками
+#include "Zad1.h"	// заголовочный файл с написанным функциями; 
 
 using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "ru");
+
+
+				// Первое задание
 	vector<bool> f = { 0, 1, 0 , 0, 1, 1, 0, 0 };
 	cout << num_of_args(f) << endl;
-	vector <bool> f1 = read_from_file("D:\\vector.txt");
-	int size = read_from_file("D:\\vector.txt").size();
-	for (int i = 0; i < size; i++) cout << f1[i] << endl;
+	vector <bool> f1 = read_from_file("D:\\tester.txt");
+	int size = read_from_file("D:\\tester.txt").size();
+	cout << endl;
+
+
+
+				// Второе задание
+	// Римские числа
+	string roman_number;
+	getline(cin, roman_number);
+	cout << "Введено: MCMXCIV" << endl;
+	cout << roman_to_arab("MCMXCIV") << endl;
+
+	// Треугольник Паскаля
+	cout << "введите число k для треугольника Паскаля: k = 4" << endl;
+	int k = 4;
+	func_Pascal(k);
+	cout << endl;
+
+	// Строки
+	cout << func_substr_len("ffffff");
+
+
+	return 0;
 }
